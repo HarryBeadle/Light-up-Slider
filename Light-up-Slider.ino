1e9216636ac3dd4dab971c2c3f4431842bb4af91
Harry Beadle <harry.beadle@gmail.com>
2014-10-01 20:16:28 +0100

    Initial Commit

---
+++ Light-up-Slider.ino
@@ -0,0 +1,26 @@
+// LEDs light up with a pot
+
+// LEDs on pins 2 though 7
+// Pot on pin A0
+
+void setup() {
+  for (int x = 2; x <= 7; x++) {
+    pinMode(x, OUTPUT);
+  }
+  pinMode(A0, INPUT);
+}
+
+int Lights = 0;
+int Read = 0;
+
+void loop() {
+  Read = analogRead(A0);
+  Lights = map(Read, 0, 1023, 1, 7);
+  for (int i = 2; i <= Lights; i++) {
+    digitalWrite(i, HIGH);
+  }
+  for (int i = 7; i > Lights; i--) {
+    digitalWrite(i, LOW);
+  }
+}
+
